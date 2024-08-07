84b4957b44153aebba26ca8c31cac1a3b04fbd22
Lucas L <llavarello@kulkan.com>
2024-08-07 12:17:46 -0300

    Moving a few association results to verbose mode

--- src/gitxray/xrays/association_xray.py
+++ src/gitxray/xrays/association_xray.py
@@ -8,24 +8,14 @@ def run(gx_context, gx_output):
     for colType, colValue in collisions:
         affected_accounts = collisions.get((colType, colValue))
         if not affected_accounts: continue
+        msg = None
 
-        if colType == "PGP_KEYID":
-            if colValue in gx_definitions.GITHUB_WEB_EDITOR_SIGNING_KEYS:
-                msg = f"GitHub's Web Editor (because of GitHub PGP Key IDs) was used by accounts: {affected_accounts}."
-            else:
+        if colType == "PGP_KEYID" and (colValue not in gx_definitions.GITHUB_WEB_EDITOR_SIGNING_KEYS):
                 msg = f"A Personal/Private PGP Key with ID {colValue} found shared by accounts: {affected_accounts}."
-        elif colType == "PGP_PKA":
-            msg = f"PGP Public Key Algorithm ({colValue}) shared by accounts: {affected_accounts}."
-        elif colType == "PGP_SIG_TYPE":
-            msg = f"PGP Signature Type ({colValue}) shared by accounts: {affected_accounts}."
-        elif colType == "PGP_HA":
-            msg = f"PGP Hash Algorithm ({colValue}) shared by accounts: {affected_accounts}."
         elif colType == "PGP_SCT":
             msg = f"PGP Signature Creation Time ({colValue}) shared by accounts: {affected_accounts}."
         elif colType == "PGP_SUBKEY_CREATED_AT":
             msg = f"The following contributor accounts have PGP Subkeys that were created in the same day: {affected_accounts}."
-        elif colType == "SSH_SA":
-            msg = f"SSH Signature Algorithm ({colValue}) shared by accounts: {affected_accounts}."
         elif colType == "SSH_SIGNING_KEY_CREATED_AT":
             msg = f"The following contributor accounts have SSH signing keys that were created in the same day: {affected_accounts}."
         elif colType == "KEY_ARMORED_VERSION":
@@ -36,10 +26,26 @@ def run(gx_context, gx_output):
             msg = f"Email {colValue} shared by accounts: {affected_accounts}."
         elif colType == "DAYS_SINCE_CREATION":
             msg = f"The following contributor accounts were created in the same day, precisely {colValue} days ago: {affected_accounts}."
-        elif colType == "DAYS_SINCE_UPDATED":
-            msg = f"The following contributor accounts were last updated in the same day, precisely {colValue} days ago: {affected_accounts}."
 
         # Let's only add the result to the repository group
-        gx_output.r_log(msg, rtype="association")
+        if msg != None: gx_output.r_log(msg, rtype="association")
+        msg = None
+
+        # Let's keep a group of associations under verbose only
+        if gx_output.verbose_enabled():
+            if colType == "DAYS_SINCE_UPDATED" :
+                msg = f"The following contributor accounts were last updated in the same day, precisely {colValue} days ago: {affected_accounts}."
+            elif colType == "PGP_HA":
+                msg = f"PGP Hash Algorithm ({colValue}) shared by accounts: {affected_accounts}."
+            elif colType == "PGP_PKA":
+                msg = f"PGP Public Key Algorithm ({colValue}) shared by accounts: {affected_accounts}."
+            elif colType == "PGP_KEYID" and colValue in gx_definitions.GITHUB_WEB_EDITOR_SIGNING_KEYS:
+                msg = f"GitHub's Web Editor (Key ID: {colValue}) was used by accounts: {affected_accounts}."
+            elif colType == "PGP_SIG_TYPE":
+                msg = f"PGP Signature Type ({colValue}) shared by accounts: {affected_accounts}."
+            elif colType == "SSH_SA":
+                msg = f"SSH Signature Algorithm ({colValue}) shared by accounts: {affected_accounts}."
+
+        if msg != None: gx_output.r_log(msg, rtype="association")
 
     return True
